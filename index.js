fc9b542dd09f7fcaaa4f264aa77441915f1b74d2
FacuVerge <facu.verge@gmail.com>
2024-08-30 12:15:21 +0200

    feat: part 3 finally completed

--- index.js
+++ index.js
@@ -14,39 +14,33 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/info', (request, response, next) => {
-	response.send(
-		'<p>Phonebook has info for ' + persons.length + ' people</p>' +
-		'<p>' + new Date() + '</p>'
-	)
-})
-
 app.get('/api/persons', (request, response, next) => {
 	Person.find({}).then(persons => {
-	  	response.json(persons)
+		response.json(persons)
 	}).catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
 	Person.findById(request.params.id)
-	.then(person => {
-		response.json(person) 
-	})     
-	.catch(error => next(error))
+		.then(person => {
+			response.json(person)
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
+
 	const body = request.body
-  
+
 	if (!body.name) {
-		return response.status(400).json({ 
-			error: 'Name is missing' 
+		return response.status(400).json({
+			error: 'Name is missing'
 		})
 	}
 
 	if (!body.phone) {
-		return response.status(400).json({ 
-			error: 'Phone is missing' 
+		return response.status(400).json({
+			error: 'Phone is missing'
 		})
 	}
 
@@ -54,7 +48,7 @@ app.post('/api/persons', (request, response, next) => {
 		name: body.name,
 		phone: body.phone,
 	})
-  
+
 	person.save().then(savedPerson => {
 		response.json(savedPerson)
 	}).catch(error => next(error))
@@ -63,10 +57,10 @@ app.post('/api/persons', (request, response, next) => {
 
 app.put('/api/persons/:id', (request, response, next) => {
 	const { name, phone } = request.body
-  
+
 	Person.findByIdAndUpdate(
-		request.params.id, 
-		{ name, phone }, 
+		request.params.id,
+		{ name, phone },
 		{ new: true, runValidators: true, context: 'query' }
 	)
 		.then(updatedPerson => {
@@ -76,7 +70,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-	Person.deleteOne({_id: request.params.id}).then(
+	Person.deleteOne({ _id: request.params.id }).then(
 		response.status(204).end()
 	).catch(error => next(error))
 })
@@ -89,16 +83,16 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message)
-  
+
 	if (error.name === 'CastError') {
-	  	return response.status(400).send({ error: 'Malformatted id' })
-	} else if (error.name === 'ValidationError') {    
-		return response.status(400).json({ error: error.message })  
+		return response.status(400).send({ error: 'Malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
 	}
-  
+
 	next(error)
 }
-  
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 const url =
   `mongodb+srv://facuverge:${password}@full-stack-course.i2ong.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Full-Stack-Course`
@@ -9,38 +9,35 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
+	name: String,
+	phone: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 switch(process.argv.length) {
-    case 3:
-        Person.find({}).then(result => {
-            console.log('Phonebook:')
-            result.forEach(person => {
-                console.log(person.name, person.phone)
-            })
-            mongoose.connection.close()
-        })
+case 3:
+	Person.find({}).then(result => {
+		console.log('Phonebook:')
+		result.forEach(person => {
+			console.log(person.name, person.phone)
+		})
+		mongoose.connection.close()
+	})
+	break
+case 5:
+	var person = new Person({
+		name: process.argv[3],
+		phone: process.argv[4],
+	})
+	person.save().then(result => {
 
-        break;
-    case 5:
-        const person = new Person({
-            name: process.argv[3],
-            phone: process.argv[4],
-        })
-
-        person.save().then(result => {
-
-            console.log(`Added ${result.name} name ${result.phone} to phonebook!`)
-            mongoose.connection.close()
-        })
-
-        break;
-    default:
-        console.log('Bad arguments')
-        process.exit(1)
+		console.log(`Added ${result.name} name ${result.phone} to phonebook!`)
+		mongoose.connection.close()
+	})
+	break
+default:
+	console.log('Bad arguments')
+	process.exit(1)
 }
 
